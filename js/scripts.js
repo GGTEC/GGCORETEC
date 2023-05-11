29d2b63bffd482d866549ac3c0703284d90f5b56
GG_TEC <jheferson1gambet@gmail.com>
2023-05-11 11:03:41 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -100,7 +100,7 @@ fetch('https://ggtec.github.io/GGTECApps/posts/posts.json')
           <div class="small text-muted">${firstPost.post_date}</div>
           <h2 class="card-title post-title">${firstPost.post_title}</h2>
           <p class="card-text">${firstPost.post_content_preview}</p>
-          <a class="btn btn-purple" href="https://ggtec.netlify.app/?p=${post_title_url = post.post_title.replace(/\s+/g, "+")}" data-post-index="${0}">Leia mais <i class="fa-solid fa-right-long"></i></a>
+          <a class="btn btn-purple" href="https://ggtec.netlify.app/?p=${post_title_url = firstPost.post_title.replace(/\s+/g, "+")}" data-post-index="${0}">Leia mais <i class="fa-solid fa-right-long"></i></a>
         </div>
       </div>
     `;
