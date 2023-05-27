d33ba0f70bbd343d5fb8c4a696378f69e485de66
GG_TEC <jheferson1gambet@gmail.com>
2023-05-27 14:00:01 -0300

    Update 5.6.0

--- assets/web/src/html/emote/emote.js
+++
@@ -1,6 +0,0 @@
-
-                const emojis = document.querySelectorAll('#emojis img');
-                emojis.forEach((emoji) => {
-                emoji.style.left = `calc(100% * ${Math.random()})`;
-                });
-            
