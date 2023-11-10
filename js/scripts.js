32daf4872e8ced10d3a3701061e876a0bbd4b4bb
GG_TEC <jheferson1gambet@gmail.com>
2023-11-10 16:31:23 -0300

    Fix Versions List

--- js/scripts.js
+++ js/scripts.js
@@ -36,6 +36,7 @@ $(window).on("load", function () {
 
   var repoOwner = 'GGTEC'
   var repoName = 'RewardEvents'
+  var repoNameVB = 'VibesBot'
   
   fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/releases`)
     .then(response => response.json())
@@ -66,6 +67,43 @@ $(window).on("load", function () {
           counter++;
         });
         let totalDownloadsEl = document.getElementById("total-downloads");
+        totalDownloadsEl.innerHTML = `
+        <h2>Total de Downloads: ${totalDownloads}</h2>
+      `;
+      }
+      
+    })
+    .catch(error => console.error(error));
+
+    fetch(`https://api.github.com/repos/${repoOwner}/${repoNameVB}/releases`)
+    .then(response => response.json())
+    .then(data => {
+
+      let releasesList = document.querySelector("#releases-list-vb");
+
+      if (releasesList != undefined){
+        let counter = 0;
+        let totalDownloads = 0;
+        data.forEach(release => {
+          release.assets.forEach(asset => {
+            totalDownloads += asset.download_count;
+          });
+          if (counter === 7) {
+            return;
+          }
+          let releaseEl = document.createElement("div");
+          let truncatedBody = truncateText(release.body, 100);
+          releaseEl.classList.add('version_block')
+          releaseEl.setAttribute('title', `Abrir pagina da versão`)
+          releaseEl.setAttribute('onclick', `window.open("${release.html_url}")`)
+          releaseEl.innerHTML = `
+          <p>Versão: ${release.tag_name}</p>
+          <p class='version_text'>${truncatedBody}</p>
+        `;
+          releasesList.appendChild(releaseEl);
+          counter++;
+        });
+        let totalDownloadsEl = document.getElementById("total-downloads-vb");
         totalDownloadsEl.innerHTML = `
         <h2>Total de Downloads: ${totalDownloads}</h2>
       `;
